20109410ecef0da3b68e1d05914d71d85f6ae5b0
Hugo Charré <hugo.charre@vercel.com>
2024-10-18 16:40:57 -0400

    test ssg

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,17 +1,26 @@
 import { Button } from "@/components/ui/button"
 import Link from "next/link"
 import Image from "next/image"
-import Carousel from "@/components/carousel"
 
-export default function Landing() {
+async function getHomepageData() {
+  const res = await fetch('https://conf-24-rendering-api.vercel.app/api/homepage');
+  if (!res.ok) {
+    throw new Error('Failed to fetch homepage data');
+  }
+  return res.json();
+}
+
+export default async function Landing() {
+  const { header, subheader } = await getHomepageData();
+
   return (
     <div className="flex flex-col md:flex-row items-center justify-between">
       <div className="md:w-1/2 mb-10 md:mb-0">
         <h1 className="text-4xl md:text-6xl font-bold mb-6 leading-tight">
-          Streamline your workflow with Cube
+          {header}
         </h1>
         <p className="text-xl mb-10 text-gray-300">
-          Boost productivity and simplify collaboration with our innovative Quantum 3D SaaS platform.
+          {subheader}
         </p>
         <Link href="/dashboard" prefetch>
           <Button size="lg" className="bg-white text-black hover:bg-gray-200 rounded-full px-8 py-3 text-lg font-semibold">
