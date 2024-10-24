51b0043b13b7c25b089715a0e0313ad50daac4e6
Hugo Charré <hugo.charre@vercel.com>
2024-10-24 00:41:57 -0700

    cheats

---
+++ components/gallery/carousel-cheat.tsx
@@ -0,0 +1,19 @@
+import { CarouselContent, AIGeneration } from './carousel-content'
+
+async function getGenerations(): Promise<AIGeneration[]> {
+  const res = await fetch('https://conf-24-rendering-api.vercel.app/api/gallery', {
+    next: { revalidate: 10 } // Revalidate every 10 seconds
+  })
+
+  if (!res.ok) {
+    throw new Error('Failed to fetch AI generations')
+  }
+
+  return res.json()
+}
+
+export default async function Carousel() {
+  const generations = await getGenerations()
+
+  return <CarouselContent generations={generations} />
+}
